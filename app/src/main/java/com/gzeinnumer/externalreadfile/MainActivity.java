8db965a70d4dc7c3ab6a265824edc9891ac407ec
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-07-30 09:05:12 +0700

    validasi apakah file sudah dibuat

--- app/src/main/java/com/gzeinnumer/externalreadfile/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/externalreadfile/MainActivity.java
@@ -27,13 +27,30 @@ public class MainActivity extends AppCompatActivity {
         tv = findViewById(R.id.tv);
 
         if (FunctionGlobalDir.isFileExists(FunctionGlobalDir.appFolder+"/File.txt")){
-            msg+="File sudah dibuat sebelumnya ";
+            msg+="File sudah dibuat sebelumnya \n";
             tv.setText(msg);
-        } else {
-            FunctionGlobalFile.initFile("Text yang dibuat di file");
             List<String> list = FunctionGlobalFile.readFile(FunctionGlobalDir.appFolder+"/File.txt");
-            msg+="Jumlah line di file ada "+ list.size();
-            tv.setText(msg);
+            if (list !=null){
+                msg+="Jumlah line di file ada "+ list.size();
+                tv.setText(msg);
+            } else {
+                msg+="Gagal membaca data";
+                tv.setText(msg);
+            }
+        } else {
+            if(FunctionGlobalFile.initFile("Text yang dibuat di file")){
+                List<String> list = FunctionGlobalFile.readFile(FunctionGlobalDir.appFolder+"/File.txt");
+                if (list !=null){
+                    msg+="Jumlah line di file ada "+ list.size();
+                    tv.setText(msg);
+                } else {
+                    msg+="Gagal membaca data";
+                    tv.setText(msg);
+                }
+            } else {
+                msg+="File gagal dibuat ";
+                tv.setText(msg);
+            }
         }
     }
 }
--- app/src/main/java/com/gzeinnumer/externalreadfile/helper/FunctionGlobalFile.java
+++ app/src/main/java/com/gzeinnumer/externalreadfile/helper/FunctionGlobalFile.java
@@ -13,7 +13,7 @@ public class FunctionGlobalFile {
     private static final String TAG = "FunctionGLobalFile_";
 
     //create file
-    public static void initFile(String text) {
+    public static boolean initFile(String text) {
         File file;
         file = new File(FunctionGlobalDir.getStorageCard + FunctionGlobalDir.appFolder +"/File.txt");
         try{
@@ -25,14 +25,20 @@ public class FunctionGlobalFile {
             writer.flush();
             writer.close();
             f.close();
+            return true;
         }catch (IOException e){
             e.printStackTrace();
+            return false;
         }
     }
 
     public static List<String> readFile(String path){
         if (!FunctionGlobalDir.isFileExists(path)){
-            FunctionGlobalFile.initFile("Buat file");
+            if (!FunctionGlobalFile.initFile("Buat file")){
+                FunctionGlobalDir.myLogD(TAG, "File success dibuat");
+            } else {
+                return null;
+            }
         }
 
         File file = new File(FunctionGlobalDir.getStorageCard+ FunctionGlobalDir.appFolder+ "/File.txt");
